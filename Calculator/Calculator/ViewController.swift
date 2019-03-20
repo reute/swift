458924a12e46c1b528d58889c9b5110ece0d1174
reute <j.rtl@outlook.de>
2019-03-20 18:22:26 +0100

    calc init

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by carmen 1 on 20.03.19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
